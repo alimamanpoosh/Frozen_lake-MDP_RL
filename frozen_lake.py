a8218b34d8307e73d5535a100cfc915a41fe4cbf
AA00163 <AAlimohammadi00163@gmail.com>
2023-04-28 20:03:17 -0700

    impelement with mdp

--- frozen_lake.py
+++ frozen_lake.py
@@ -6,40 +6,11 @@ from gymnasium.envs.toy_text.frozen_lake import generate_random_map
 
 # Create Environment
 env = gym.make("FrozenLake-v1", desc=generate_random_map(size=16), render_mode="human", is_slippery=True)
+observation, info = env.reset(seed=42)
 action_space_size = env.action_space.n
 stat_space_size = env.observation_space.n
 qtable = np.zeros((stat_space_size, action_space_size))
-# # Define MDP components
-# num_states = env.observation_space.n
-# num_actions = env.action_space.n
-# trans_prob = np.zeros((num_states, num_actions, num_states))
-# reward = np.zeros((num_states, num_actions))
 
-# # Calculate Transition Probabilities and Rewards(Value iteration)
-# for s in range(num_states):
-#     for a in range(num_actions):
-#         transitions = env.env.P[s][a]
-#         for p_trans, next_s, r, done in transitions:
-#             trans_prob[s, a, next_s] += p_trans
-#             reward[s, a] += r * p_trans
-#
-# # Normalize Transition Probabilities
-# for s in range(num_states):
-#     for a in range(num_actions):
-#         trans_prob[s, a] /= np.sum(trans_prob[s, a])
-#
-#
-# # Define Policy Function
-# def policy(state, q_values, epsilon):
-#     if np.random.uniform() < epsilon:
-#         # Random Action
-#         return np.random.randint(num_actions)
-#     else:
-#         # Greedy Action
-#         return np.argmax(q_values[state])
-
-
-# Define Q-Learning Algorithm(Q Function)
 num_episodes = 1000
 learning_rate = 0.2
 alpha = 0.1
@@ -53,6 +24,7 @@ max_iter_number = 1000
 rewards = []
 
 for episode in range(num_episodes):
+    print(episode)
     state = env.reset()[0]
     step = 0
     done = False
@@ -66,35 +38,17 @@ for episode in range(num_episodes):
         new_state, reward, done1, done, info = env.step(action)
         max_new_state = np.max(qtable[new_state, :])
         qtable[state, action] = qtable[state, action] + learning_rate * (
-            int(reward) + gamma * int(max_new_state) - qtable[state, action])
+            reward + gamma * max_new_state - qtable[state, action])
         total_rewards += reward
         state = new_state
-        if done:
+        if done or done1:
             break
     epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay_rate * episode)
     rewards.append(total_rewards)
 
 print("score:", str(sum(rewards) / total_rewards))
 
-# q_values = np.zeros((num_states, num_actions))
-# for i in range(num_episodes):
-#     state = env.reset()
-#     done = False
-#     while not done:
-#         action = policy(state, q_values, epsilon)
-#         next_state, r, done, _ = env.step(action)
-#         q_values[state, action] += alpha * (r + gamma * np.max(q_values[next_state]) - q_values[state, action])
-#         state = next_state
-#
-# # Run Trained Agent
-# observation, info = env.reset()
-# for i in range(max_iter_number):
-#     action = policy(observation, q_values, epsilon=0.0)
-#     observation, reward, done, info = env.step(action)
-#     if done:
-#         observation, info = env.reset()
-#
-# env.close()
+
 env.reset()
 
 for episode in range(1):
